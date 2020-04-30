b8fa543dd3863df020d12b02ae99fed3bc61318a
paulanguedes <paulanguedes@gmail.com>
2020-04-30 20:34:57 -0300

    página de login e botão de voltar ao login

---
+++ php/login.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link type="text/css" rel="stylesheet" href="../css/materialize.min.css" />
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+<body id="paginalogin">
+
+  <section class="login">
+
+    <form class="container center">
+      
+      <div class="center">
+        <h5>login</h5>
+      </div>
+
+      <div class="input-field">
+        <input id="email" type="email" class="validate">
+        <label for="email">E-mail</label>
+      </div>
+
+      <div class="input-field">
+        <input id="password" type="password" class="validate">
+        <label for="password">Senha</label>
+      </div>
+
+      <button class="btn waves-effect waves-light" type="submit" name="action">Entrar
+        <i class="material-icons right">send</i>
+      </button>
+
+    </form>
+
+  </section>
+
+<script src="../js/jQuery341.js"></script>
+<script src="../js/materialize.min.js"></script>
+<script src="../js/index.js"></script>
+</body>
+</html>
