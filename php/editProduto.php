e0d0e3b8403b162c37708a760eb3b1116817aae5
paulanguedes <paulanguedes@gmail.com>
2020-05-12 17:46:11 -0300

    pagina de editar produto feita, funçao ainda nao funciona

--- php/editProduto.php
+++ php/editProduto.php
@@ -1,13 +1,20 @@
 <?php
 session_start();
-include('functons.php');
+include('functions.php');
 
-// == ACESSO SOMENTE A USUARIO LOGADO ==
+// == TESTAR SE O USUÁRIO TEM PERMISSÃO DE ACESSO ==
 if (!$_SESSION) {
   header('location: index.php');
 }
 
+//Capturar o id do produto solicitado
+$id = $_GET['id'];
+
+// Carregar as informações do produto que tem o id
+$produto = produtoID($id);
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -23,12 +30,12 @@ if (!$_SESSION) {
   <div class="home">
     <nav>
       <div class="nav-wrapper">
-        <a href="#" class="brand-logo"><img width="100px" src="../img/Logo2.png" alt="Logo do site"></a>
+        <a href="home.php" class="brand-logo"><img width="100px" src="../img/Logo2.png" alt="Logo do site"></a>
         <div class="nav-icons">
           <ul class="nav-icons-list tabs right hide-on-med-and-down">
-            <li class="icon-name tab col s3 tooltipped"><a href="produtos.php"><i class="material-icons tooltipped" data-position="bottom" data-tooltip="doces">view_module</i>Hover me!</a></li>
-            <li class="icon-name tab col s3 tooltipped"><a href="createProduto.php"><i class="material-icons tooltipped" data-position="bottom" data-tooltip="adicionar doce">library_add</i>Hover me!</a></li>
-            <li class="icon-name tab col s3 tooltipped"><a href="createUsuario.php"><i class="material-icons tooltipped" data-position="bottom" data-tooltip="adicionar usuário">person_add</i>Hover me!</a></li>
+            <li class="icon-name tab col s3 tooltipped"><a href="produtos.php"><i class="material-icons tooltipped" data-position="bottom" data-tooltip="lista de produtos">view_module</i>Hover me!</a></li>
+            <li class="icon-name tab col s3 tooltipped"><a href="createProduto.php"><i class="material-icons tooltipped" data-position="bottom" data-tooltip="novo produto">library_add</i>Hover me!</a></li>
+            <li class="icon-name tab col s3 tooltipped"><a href="createUsuario.php"><i class="material-icons tooltipped" data-position="bottom" data-tooltip="novo usuário">person_add</i>Hover me!</a></li>
             <li class="icon-name tab col s3 tooltipped"><a href="index.php"><i class="material-icons tooltipped" data-position="bottom" data-tooltip="encerrar sessão">face</i></a></li>
           </ul>
         </div>
@@ -36,20 +43,59 @@ if (!$_SESSION) {
     </nav>
   </div>
 
-  <section class="editProduto">
+  <section class="editProduto container center">
+  
+      <div class="card">
+        <img class="logoreceita" src="../img/Logo2.png" alt="Logo Fatias de Memória">
 
-    
+        <div class="card-content row">
+          <div class="input-field col s12">
+            <input value="<?= $produto['produto'] ?>" id="first_name2" type="text" class="validate">
+            <label class="active" for="first_name2">nome</label>
+          </div>
+        </div>
 
+        <div class="card-content row">
+          <div class="input-field col s12">
+            <input value="<?= $produto['descricao'] ?>" id="first_name2" type="text" class="validate">
+            <label class="active" for="first_name2">sobre o doce</label>
+          </div>
+        </div>
+
+        <div class="card-content row">
+          <div class="input-field col s12">
+            <input value="R$ <?= number_format($produto['preco'], 2, ',', '.') ?> " id="first_name2" type="number" class="validate">
+            <label class="active" for="first_name2">preço</label>
+          </div>
+        </div>
+
+        <div class="file-field card-content input-field">
+          <div class="btn">
+            <span>imagem</span>
+            <input type="file">
+          </div>
+          <div class="file-path-wrapper">
+            <input class="file-path validate" type="text">
+          </div>
+        </div>
+        
+        <div class="card-image">
+          <img src=' <?= $produto['foto'] ?> '>
+        </div>
+
+        <div class="card-action">
+          <a href="#" class="waves-effect waves-light btn">editar</a>
+          <a href="#" class="waves-effect waves-light btn">deletar</a>
+        </div>
+      </div>
+  
   </section>
 
   <footer class="page-footer">
-
     <div class="container">
-
       <div class="socialtitle">
         <h5>Siga-nos nas redes sociais</h5>
       </div>
-
       <div class="sociallinks">
         <ul>
           <li><a href="#!">Facebook</a></li>
@@ -58,15 +104,11 @@ if (!$_SESSION) {
           <li><a href="#!">Twitter</a></li>
         </ul>
       </div>
-      
     </div>
-
     <div class="footer-copyright">
       <div class="container">&copy; 2020 Copyright Text</div>
     </div>
-
   </footer>
-
 
 <script src="../js/materialize.min.js"></script>
 <script src="../js/index.js"></script>
